8dced6a03234c6930b49cc3b4736eed03c3950cb
onyu <gkc04075@gmail.com>
2024-09-12 17:25:38 +0900

    [FE] feat/공통모달, 아이콘 생성

--- app/src/main/java/com/example/muincctv/LoginActivity.kt
+++ app/src/main/java/com/example/muincctv/LoginActivity.kt
@@ -21,19 +21,27 @@ class LoginActivity : AppCompatActivity() {
             insets
         }
 
-        // 확인 버튼 클리 시 아이디/비번찾기 화면으로 이동
+        // 버튼 클리 시 아이디/비번찾기 화면으로 이동
         val findIDPWBtn = findViewById<TextView>(R.id.loading_find_id)
         findIDPWBtn.setOnClickListener {
             val intent = Intent(this, Find_Id_PwActivity::class.java)
             startActivity(intent)
         }
 
-        // 확인 버튼 클리 시 아이디/비번찾기 화면으로 이동
+        // 버튼 클리 시 회원가입 화면으로 이동
         val goSignUpBtn = findViewById<AppCompatButton>(R.id.go_signup_btn)
         goSignUpBtn.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
 
+        // 로그인 버튼 클리 시 메인 화면으로 이동
+        val loadingLoginBtn = findViewById<AppCompatButton>(R.id.loading_login_btn)
+        loadingLoginBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
+

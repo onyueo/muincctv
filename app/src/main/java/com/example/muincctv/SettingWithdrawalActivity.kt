c42f13c52eae5fa0515875da38cdb7f50253b6a0
onyu <gkc04075@gmail.com>
2024-10-08 17:30:28 +0900

    feat: 탈퇴하기 페이지 작업

---
+++ app/src/main/java/com/example/muincctv/SettingWithdrawalActivity.kt
@@ -0,0 +1,32 @@
+package com.example.muincctv
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.muincctv.databinding.ActivitySettingWithdrawlBinding
+
+class SettingWithdrawalActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySettingWithdrawlBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_setting_withdrawl)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // appbar 타이틀 변경
+        binding.titleText = "탈퇴하기"
+
+    }
+}

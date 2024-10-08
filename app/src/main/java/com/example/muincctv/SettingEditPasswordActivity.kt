b34b7cec2468f4ab45338a11378fb93a5faa1bdb
onyu <gkc04075@gmail.com>
2024-10-08 14:37:37 +0900

    feat: 설정 - 비밀번호 변경 페이지

--- app/src/main/java/com/example/muincctv/SettingEditPasswordActivity.kt
+++ app/src/main/java/com/example/muincctv/SettingEditPasswordActivity.kt
@@ -5,16 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.muincctv.databinding.ActivitySettingEditPasswordBinding
 
 class SettingEditPasswordActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingEditPasswordBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_setting_edit_password)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_setting_edit_password)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // appbar 타이틀 변경
+        binding.titleText = "비밀번호 변경"
+
+
+
     }
 }

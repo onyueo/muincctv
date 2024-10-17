b9b3e054d8478f2dd5f278bfc2e17a9314de514e
onyu <gkc04075@gmail.com>
2024-10-17 13:23:28 +0900

    design: 기기 이상 확인 layout 추가

--- app/src/main/java/com/example/muincctv/RecordingListActivity.kt
+++ app/src/main/java/com/example/muincctv/RecordingListActivity.kt
@@ -1,7 +1,9 @@
 package com.example.muincctv
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -15,12 +17,9 @@ import com.example.muincctv.databinding.ActivityRecordingListBinding
 
 class RecordingListActivity : AppCompatActivity() {
 
-    private lateinit var recordingDropdownDevice: Spinner
-    private lateinit var recordingDropdownGroup: Spinner
-
     private lateinit var recyclerView: RecyclerView
-
     private lateinit var binding: ActivityRecordingListBinding
+    private lateinit var recordingAdapter: RecordingListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,21 +42,29 @@ class RecordingListActivity : AppCompatActivity() {
             onBackPressed()
         }
 
-        // 드롭다운 설정
-        recordingDropdownDevice = findViewById(R.id.recording_dropdown_device)
-        recordingDropdownGroup = findViewById(R.id.recording_dropdown_group)
 
-        val devices = arrayListOf(
-            "Device 1",
-            "Device 2",
-            "Device 3"
-        )
+// 드롭다운 설정
+        val recordingDropdownGroup = findViewById<TextView>(R.id.recording_dropdown_group)
+        val recordingDropdownTime = findViewById<TextView>(R.id.recording_dropdown_time)
 
-        val groups = arrayListOf(
-            "Group A",
-            "Group B",
-            "Group C"
-        )
+        // 데이터 배열
+        val devices = listOf("현관", "거실", "거실2", "안방 화장실")
+        val devicesList = convertToMainDropDownModel(devices)
+
+        val groups = listOf("오늘", "일주일", "1개월", "3개월", "6개월", "12개월")
+        val groupsList = convertToMainDropDownModel(groups)
+
+        recordingDropdownGroup.setOnClickListener {
+            CustomDialog(this, ArrayList(devicesList)) { selectedItem ->
+                recordingDropdownGroup.text = selectedItem.device_title
+            }.show()
+        }
+
+        recordingDropdownTime.setOnClickListener {
+            CustomDialog(this, ArrayList(groupsList)) { selectedItem ->
+                recordingDropdownTime.text = selectedItem.device_title
+            }.show()
+        }
 
 
 
@@ -71,11 +78,29 @@ class RecordingListActivity : AppCompatActivity() {
         )
 
         recyclerView = findViewById<RecyclerView>(R.id.recording_RV)
-        val recordingAdapter = RecordingListAdapter(items)
+        recordingAdapter = RecordingListAdapter(items)
         recyclerView.adapter = recordingAdapter
         recyclerView.layoutManager = LinearLayoutManager(this)
 
 
+        // 녹화영상 여부 UI업데이트
+        updateUI(items)
 
     }
+
+    private fun updateUI(items: List<RecordingListModel>) {
+        // 리사이클러뷰의 데이터 길이에 따라 가시성 설정
+        if (items.isEmpty()) {
+            binding.noRecordingText.visibility = View.VISIBLE
+            binding.recordingRV.visibility = View.GONE
+        } else {
+            binding.noRecordingText.visibility = View.GONE
+            binding.recordingRV.visibility = View.VISIBLE
+        }
+    }
+
+    fun convertToMainDropDownModel(items: List<String>): List<MainDropDownModel> {
+        return items.map { MainDropDownModel(device_title = it, device_choice = "") }
+    }
+
 }
